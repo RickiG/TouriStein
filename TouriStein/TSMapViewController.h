f54f979faab87b6cfa8ca840bba52f5638c7e447
Ricki Gregersen <hi@rickigregersen.com>
2014-05-16 12:53:01 +0200

    added a mapViewController

---
+++ TouriStein/TSMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  TSMapViewController.h
+//  TouriStein
+//
+//  Created by Ricki Gregersen on 16/05/14.
+//  Copyright (c) 2014 TouriStein 3D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSMapViewController : UIViewController
+
+@end
