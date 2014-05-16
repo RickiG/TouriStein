fc2a7901c5268534efcea533974856f6bb0b4f5e
Ricki Gregersen <hi@rickigregersen.com>
2014-05-16 11:29:53 +0200

    Initial Commit

---
+++ TouriStein/TSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TSAppDelegate.h
+//  TouriStein
+//
+//  Created by Ricki Gregersen on 16/05/14.
+//  Copyright (c) 2014 TouriStein 3D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TouriStein/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TouriStein
+//
+//  Created by Ricki Gregersen on 16/05/14.
+//  Copyright (c) 2014 TouriStein 3D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TSAppDelegate class]));
+    }
+}
